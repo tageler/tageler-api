0a8bb9f0b968f90f59d4a38d3af696bc0338d43d
Kevin Meister <kevin.meister@students.unibe.ch>
2017-04-01 16:55:13 +0200

    added quick fix to the quick fix

--- test/tageler.js
+++ test/tageler.js
@@ -154,10 +154,10 @@ describe('List of tageler', function() {
                 .expect('Content-Type', /json/)
                 .end(function(err, res) {
                     console.log(res.toString());
-                    if (i == tageler.length-1){
-                        done();
-                    }
                 });
+            if (i === (tageler.length-1)){
+                done();
+            }
         }
 
     });
